cd5aff3d2340fdb0862dfc46a4294dd0be5d6ccb
HosseinBeheshti <h.beheshti92@gmail.com>
2022-02-02 15:29:00 +0330

    add fsbl pmufw

--- tcl/create_sdk_project.tcl
+++ tcl/create_sdk_project.tcl
@@ -16,11 +16,19 @@ setws ${origin_dir}/build/apu/
 #-----------------
 platform create -name "zcu208_platform" -hw ${prj_dir}/${top_name}.xsa -proc psu_cortexa53_0 -os standalone 
 platform generate
+#---------------------------
+# create fsbl from template
+#---------------------------
+app create -name a53_fsbl -template {Zynq MP FSBL} -hw ${prj_dir}/${top_name}.xsa -proc psu_cortexa53_0  -os standalone
+#---------------------------
+# create pmufw from template
+#---------------------------
+app create -name pmu_fw -template {ZynqMP PMU Firmware} -hw ${prj_dir}/${top_name}.xsa -proc psu_pmu_0 -os standalone
 #-------------------
 # create application 
 #-------------------
 app create -name "zcu208_init" -platform zcu208_platform
-importsources -name ${app_name} -path ./sdk_src/ -linker-script
+importsources -name zcu208_init -path ./sdk_src/ -linker-script
 #--------------
 # build project
 #--------------
