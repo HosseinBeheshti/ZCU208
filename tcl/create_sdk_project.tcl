04f13fc13aa0c205aa0343fe234ade1743ddddbc
HosseinBeheshti <h.beheshti92@gmail.com>
2022-02-03 14:20:48 +0330

    add reference files

--- tcl/create_sdk_project.tcl
+++ tcl/create_sdk_project.tcl
@@ -16,14 +16,6 @@ setws ${origin_dir}/build/apu/
 #-----------------
 platform create -name "zcu208_platform" -hw ${prj_dir}/${top_name}.xsa -proc psu_cortexa53_0 -os standalone 
 platform generate
-#---------------------------
-# create fsbl from template
-#---------------------------
-app create -name a53_fsbl -template {Zynq MP FSBL} -hw ${prj_dir}/${top_name}.xsa -proc psu_cortexa53_0  -os standalone
-#---------------------------
-# create pmufw from template
-#---------------------------
-app create -name pmu_fw -template {ZynqMP PMU Firmware} -hw ${prj_dir}/${top_name}.xsa -proc psu_pmu_0 -os standalone
 #-------------------
 # create application 
 #-------------------
