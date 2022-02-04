38c1ee111b40455557f89bdb6f9db7adb2d87fd4
HosseinBeheshti <h.beheshti92@gmail.com>
2022-02-04 23:57:35 +0330

    rename platform

--- tcl/create_sdk_project.tcl
+++ tcl/create_sdk_project.tcl
@@ -14,12 +14,12 @@ setws ${origin_dir}/build/ps/
 #-----------------
 # create platform 
 #-----------------
-platform create -name "zcu208_platform" -hw ${prj_dir}/${top_name}.xsa -proc psu_cortexa53_0 -os standalone 
+platform create -name "zcu208_mts_platform" -hw ${prj_dir}/${top_name}.xsa -proc psu_cortexa53_0 -os standalone 
 platform generate
 #-------------------
 # create application 
 #-------------------
-app create -name "zcu208_init" -platform zcu208_platform
+app create -name "zcu208_init" -platform zcu208_mts_platform
 importsources -name zcu208_init -path ./sdk_src/ -linker-script
 #--------------
 # build project
