9dc78b268db88f39d897b1d3b505256f7972f1f2
HosseinBeheshti <h.beheshti92@gmail.com>
2022-02-07 21:25:19 +0330

    change core

--- hdl/tx_core/tx_core_wrapper.v
+++ hdl/tx_core/tx_core_wrapper.v
@@ -21,46 +21,25 @@ module tx_core_wrapper
   (
     input clock,
     input resetn,
-    input adc1_tvalid,
-    input [127:0] adc1_tdata,
-    output adc1_tready,
-    input [15:0] dds_phase_inc1,
-    input adc2_tvalid,
-    input [127:0] adc2_tdata,
-    output adc2_tready,
-    input [15:0] dds_phase_inc2,
-    input adc3_tvalid,
-    input [127:0] adc3_tdata,
-    output adc3_tready,
-    input [15:0] dds_phase_inc3,
-    input [2:0] dbg_select,
-    input dac_tready,
-    output [255:0] dac_tdata,
-    output dac_tvalid,
-    output [255:0] dbg_output_data
+    input [4:0] output_select,
+    input [15:0] lo_dds_phase_inc,
+    input [127:0] adc1_data,
+    input [127:0] adc2_data,
+    input [127:0] adc3_data,
+    output [127:0] dac_data,
+    output [127:0] dbg_output_data
   );
 
   tx_core tx_core_inst
           (
             .clock(clock),
             .resetn(resetn),
-            .adc1_tvalid(adc1_tvalid),
-            .adc1_tdata(adc1_tdata),
-            .adc1_tready(adc1_tready),
-            .dds_phase_inc1(dds_phase_inc1),
-            .adc2_tvalid(adc2_tvalid),
-            .adc2_tdata(adc2_tdata),
-            .adc2_tready(adc2_tready),
-            .dds_phase_inc2(dds_phase_inc2),
-            .adc3_tvalid(adc3_tvalid),
-            .adc3_tdata(adc3_tdata),
-            .adc3_tready(adc3_tready),
-            .dds_phase_inc3(dds_phase_inc3),
-            .output_select(dbg_select),
-            .dac_tready(dac_tready),
-            .dac_tdata(dac_tdata),
-            .dac_tvalid(dac_tvalid),
-            .dbg_output_data(dbg_output_data)
+            .output_select(output_select),
+            .lo_dds_phase_inc(lo_dds_phase_inc),
+            .adc1_data(adc1_data),
+            .adc2_data(adc2_data),
+            .adc3_data(adc3_data),
+            .dac_data(dac_data)
           );
 
 endmodule
