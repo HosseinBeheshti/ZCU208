7ac549fd77463c1ad3e33c6ddc9b7257a307e84e
HosseinBeheshti <h.beheshti92@gmail.com>
2022-02-09 09:46:51 +0330

    rx core simulation

--- matlab/rx_dsp_core/rx_dsp_core_init_hw.m
+++ matlab/rx_dsp_core/rx_dsp_core_init_hw.m
@@ -1,10 +1,28 @@
 clear;
 close all;
 clc;
-sim_time = inf;
+sim_time = 1e-3;
 %%
 fs = 4e9;
 ts = 1/fs;
-fc = 2.4e9;
-fd1 = 30e6;
-fd2 = -70e6;
+fc1 = 2.37e9;
+fc2 = 2.4e9;
+fc3 = 2.43e9;
+downsample_ratio = 8;
+f_ddc1 = 70e6;
+f_ddc2 = 100e6;
+f_ddc3 = 130e6;
+%% filter
+% All frequency values are in MHz.
+Fs = 250;  % Sampling Frequency
+
+Fpass = 5;               % Passband Frequency
+Fstop = 10;              % Stopband Frequency
+Dpass = 0.057501127785;  % Passband Ripple
+Dstop = 0.001;           % Stopband Attenuation
+dens  = 20;              % Density Factor
+% Calculate the order from the parameters using FIRPMORD.
+[N, Fo, Ao, W] = firpmord([Fpass, Fstop]/(Fs/2), [1 0], [Dpass, Dstop]);
+% Calculate the coefficients using the FIRPM function.
+lpf1_coefficient = firpm(N, Fo, Ao, W, {dens});
+
