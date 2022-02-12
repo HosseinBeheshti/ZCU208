c8e926b80e6c69d97bed052fd818ca9d42ca28d8
HosseinBeheshti <h.beheshti92@gmail.com>
2022-02-12 12:35:39 +0330

    generate files

--- matlab/rx_dsp_core/rx_dsp_core_init_hw.m
+++ matlab/rx_dsp_core/rx_dsp_core_init_hw.m
@@ -5,9 +5,6 @@ sim_time = 1e-3;
 %%
 fs = 500e6;
 ts = 1/fs;
-%% filters
-lpf1_coefficient = lpf_coefficient_gen(1);
-lpf2_coefficient = lpf_coefficient_gen(0);
 %% Hardware parameters
 ddc_phase_width = 16;
 ddc_data_width = 16;
@@ -15,7 +12,11 @@ lpf1_outpu_width = 16;
 lpf1_outpu_point = 0;
 lpf2_outpu_width = 16;
 lpf2_outpu_point = 0;
-
-
-
+%% write xilinx coef files 
+hq = lpf1; 
+set(hq,'arithmetic','fixed'); 
+coewrite(hq,10,'coef_filter1');
+hq = lpf2; 
+set(hq,'arithmetic','fixed'); 
+coewrite(hq,10,'coef_filter2');
 
