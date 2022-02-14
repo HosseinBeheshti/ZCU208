329a2407d228d7ef22b3285f73ba925ded016ca0
HosseinBeheshti <h.beheshti92@gmail.com>
2022-02-14 12:23:19 +0330

    remove fir ip cores and coefficient

--- hdl/rx_core/rx_baseband_receiver.sv
+++ hdl/rx_core/rx_baseband_receiver.sv
@@ -18,26 +18,31 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module rx_baseband_receiver
-  #(
-     parameter NUMBER_OF_PATH = 4
-   )
-   (
-     input master_clock,
-     input process_clock,
-     input resetn,
-     input [15:0] data_in,
-     input [15:0] compelex_phase_inc,
-     input [15:0] real_phase_inc,
-     output [15:0] data_out
-   );
+  (
+    input clock,
+    input resetn,
+    input [15:0] data_in,
+    input [15:0] compelex_phase_inc,
+    input [15:0] real_phase_inc,
+    (* ASYNC_REG="true" *)
+    output [15:0] data_out1_i,
+    (* ASYNC_REG="true" *)
+    output [15:0] data_out1_q,
+    (* ASYNC_REG="true" *)
+    output [15:0] data_out2,
+    (* ASYNC_REG="true" *)
+    output [15:0] data_out3
+  );
 
   genvar i;
+  parameter NUMBER_OF_PATH = 4;
+  logic process_clock;
   logic [7:0] down_counter;
   logic down_strobe;
   logic [15:0] data_in_array[NUMBER_OF_PATH];
-  (*ASYNC_REG = true*)
+  (* ASYNC_REG="true" *)
   logic [16*NUMBER_OF_PATH-1:0] data_in_down4;
-  (*ASYNC_REG = true*)
+  (* ASYNC_REG="true" *)
   logic [16*NUMBER_OF_PATH-1:0] data_in_down4_reg;
   logic [16*NUMBER_OF_PATH-1:0] dds_sin1;
   logic [16*NUMBER_OF_PATH-1:0] dds_cosin1;
@@ -46,6 +51,17 @@ module rx_baseband_receiver
   logic [2*16*NUMBER_OF_PATH-1:0] cm1_data_array;
   logic [2*16*NUMBER_OF_PATH-1:0] cm2_data_array;
   logic [2*16*NUMBER_OF_PATH-1:0] cm3_data_array;
+  logic [2*16*NUMBER_OF_PATH-1:0] lpf1_data1_array;
+  logic [2*16*NUMBER_OF_PATH-1:0] lpf1_data2_array;
+  logic [2*16*NUMBER_OF_PATH-1:0] lpf1_data3_array;
+  (* ASYNC_REG="true" *)
+  logic [15:0] data_out1_i_temp;
+  (* ASYNC_REG="true" *)
+  logic [15:0] data_out1_q_temp;
+  (* ASYNC_REG="true" *)
+  logic [15:0] data_out2_temp;
+  (* ASYNC_REG="true" *)
+  logic [15:0] data_out3_temp;
 
   always @(posedge clock)
   begin
@@ -64,6 +80,20 @@ module rx_baseband_receiver
       down_strobe <= 1;
     end
   end
+
+  BUFGCE_DIV #(
+               .BUFGCE_DIVIDE(4),
+               .IS_CE_INVERTED(1'b0),
+               .IS_CLR_INVERTED(1'b0),
+               .IS_I_INVERTED(1'b0),
+               .SIM_DEVICE("ULTRASCALE_PLUS")
+             )
+             BUFGCE_DIV_inst (
+               .O(process_clock),
+               .CE(1'b1),
+               .CLR(1'b0),
+               .I(clock)
+             );
 
   // 1 -> NUMBER_OF_PATH
   always @(posedge process_clock)
@@ -132,16 +162,81 @@ module rx_baseband_receiver
     end
   endgenerate
 
-
   // 4 path fir
+  fir_compiler_4path fir_compiler_4path_ch1_i
+                     (
+                       .clk(process_clock),
+                       .concat_data_in({cm1_data_array[111:96],cm1_data_array[79:64],cm1_data_array[47:32],cm1_data_array[15:0]}),
+                       .concat_data_out({lpf1_data1_array[111:96],lpf1_data1_array[79:64],lpf1_data1_array[47:32],lpf1_data1_array[15:0]})
+                     );
 
+  fir_compiler_4path fir_compiler_4path_ch1_q
+                     (
+                       .clk(process_clock),
+                       .concat_data_in({cm1_data_array[111+16:96+16],cm1_data_array[79+16:64+16],cm1_data_array[47+16:32+16],cm1_data_array[15+16:0+16]}),
+                       .concat_data_out({lpf1_data1_array[111+16:96+16],lpf1_data1_array[79+16:64+16],lpf1_data1_array[47+16:32+16],lpf1_data1_array[15+16:0+16]})
+                     );
+
+  fir_compiler_4path fir_compiler_4path_ch2
+                     (
+                       .clk(process_clock),
+                       .concat_data_in({cm2_data_array[111:96],cm2_data_array[79:64],cm2_data_array[47:32],cm2_data_array[15:0]}),
+                       .concat_data_out({lpf1_data2_array[111:96],lpf1_data2_array[79:64],lpf1_data2_array[47:32],lpf1_data2_array[15:0]})
+                     );
+
+  fir_compiler_4path fir_compiler_4path_ch3
+                     (
+                       .clk(process_clock),
+                       .concat_data_in({cm3_data_array[111:96],cm3_data_array[79:64],cm3_data_array[47:32],cm3_data_array[15:0]}),
+                       .concat_data_out({lpf1_data3_array[111:96],lpf1_data3_array[79:64],lpf1_data3_array[47:32],lpf1_data3_array[15:0]})
+                     );
 
   // NUMBER_OF_PATH -> 1
-  always @(posedge master_clock)
+  always @(posedge clock)
   begin
-	  data_out
+    case (down_counter)
+      0:
+        data_out1_i_temp <= lpf1_data1_array[15:0];
+      1:
+        data_out1_i_temp <= lpf1_data1_array[47:32];
+      2:
+        data_out1_i_temp <= lpf1_data1_array[79:64];
+      3:
+        data_out1_i_temp <= lpf1_data1_array[111:96];
+    endcase
+    case (down_counter)
+      0:
+        data_out1_q_temp <= lpf1_data1_array[15+16:0+16];
+      1:
+        data_out1_q_temp <= lpf1_data1_array[47+16:32+16];
+      2:
+        data_out1_q_temp <= lpf1_data1_array[79+16:64+16];
+      3:
+        data_out1_q_temp <= lpf1_data1_array[111+16:96+16];
+    endcase
+    case (down_counter)
+      0:
+        data_out2_temp <= lpf1_data2_array[15:0];
+      1:
+        data_out2_temp <= lpf1_data2_array[47:32];
+      2:
+        data_out2_temp <= lpf1_data2_array[79:64];
+      3:
+        data_out2_temp <= lpf1_data2_array[111:96];
+    endcase
+    case (down_counter)
+      0:
+        data_out3_temp <= lpf1_data3_array[15:0];
+      1:
+        data_out3_temp <= lpf1_data3_array[47:32];
+      2:
+        data_out3_temp <= lpf1_data3_array[79:64];
+      3:
+        data_out3_temp <= lpf1_data3_array[111:96];
+    endcase
+    data_out1_i <= data_out1_i_temp;
+    data_out1_q <= data_out1_q_temp;
+    data_out2 <= data_out2_temp;
+    data_out3 <= data_out3_temp;
   end
-
-
-
 endmodule
--- hdl/rx_core/rx_core.sv
+++ hdl/rx_core/rx_core.sv
@@ -25,9 +25,8 @@ module rx_core
      input clock,
      input resetn,
      input [4:0] output_select,
-     input [15:0] ddc1_phase_inc,
-     input [15:0] ddc2_phase_inc,
-     input [15:0] ddc3_phase_inc,
+     input [15:0] compelex_phase_inc,
+     input [15:0] real_phase_inc,
      input [15:0] duc1_phase_inc,
      input [15:0] duc2_phase_inc,
      input [15:0] duc3_phase_inc,
@@ -39,23 +38,68 @@ module rx_core
 
   genvar i;
   logic resetn_reg;
-  logic [16*NUMBER_OF_LINE-1:0] dac1_data_i_temp1;
-  logic [16*NUMBER_OF_LINE-1:0] dac1_data_q_temp1;
-  logic [16*NUMBER_OF_LINE-1:0] dac2_data_i_temp1;
-  logic [16*NUMBER_OF_LINE-1:0] dac2_data_q_temp1;
-  logic [16*NUMBER_OF_LINE-1:0] dac3_data_i_temp1;
-  logic [16*NUMBER_OF_LINE-1:0] dac3_data_q_temp1;
+  logic [16*NUMBER_OF_LINE-1:0] baseband_data1_i;
+  logic [16*NUMBER_OF_LINE-1:0] baseband_data1_q;
+  logic [16*NUMBER_OF_LINE-1:0] baseband_data2;
+  logic [16*NUMBER_OF_LINE-1:0] baseband_data3;
+  logic [16*NUMBER_OF_LINE-1:0] lpf2_data1_i;
+  logic [16*NUMBER_OF_LINE-1:0] lpf2_data1_q;
+  logic [16*NUMBER_OF_LINE-1:0] lpf2_data2_i;
+  logic [16*NUMBER_OF_LINE-1:0] lpf2_data2_q;
+  logic [16*NUMBER_OF_LINE-1:0] lpf2_data3_i;
+  logic [16*NUMBER_OF_LINE-1:0] lpf2_data3_q;
+
   always @(posedge clock)
   begin
     resetn_reg <= resetn;
   end
 
+rx_baseband_receiver rx_baseband_receiver_inst 
+(
+     .clock(clock),
+     .resetn(resetn_reg),
+     .data_in(adc_data[15:0]),
+     .compelex_phase_inc(compelex_phase_inc),
+     .real_phase_inc(real_phase_inc),
+     .data_out1_i(baseband_data1_i[15:0]),
+     .data_out1_q(baseband_data1_q[15:0]),
+     .data_out2(baseband_data2[15:0]),
+     .data_out3(baseband_data3[15:0])
+   );
+
+// upsample fir filter
+  fir_compiler_8path fir_compiler_8path_ch1_i
+                     (
+                       .clk(clock),
+                       .concat_data_in(baseband_data1_i),
+                       .concat_data_out(lpf2_data1_i)
+                     );
+
+  fir_compiler_8path fir_compiler_8path_ch1_q
+                     (
+                       .clk(clock),
+                       .concat_data_in(baseband_data1_q),
+                       .concat_data_out(lpf2_data1_q)
+                     );
+  fir_compiler_8path fir_compiler_8path_ch2
+                     (
+                       .clk(clock),
+                       .concat_data_in(baseband_data2),
+                       .concat_data_out(lpf2_data2_i)
+                     );
+  fir_compiler_8path fir_compiler_8path_ch3
+                     (
+                       .clk(clock),
+                       .concat_data_in(baseband_data3),
+                       .concat_data_out(lpf2_data3_i)
+                     );
+
   iq_freq_shift	iq_freq_shift_dac1
                 (
                   .clock(clock),
                   .resetn(resetn_reg),
-                  .data_in_i(dac1_data_i_temp1),
-                  .data_in_q(dac1_data_q_temp1),
+                  .data_in_i(lpf2_data1_i),
+                  .data_in_q(lpf2_data1_q),
                   .dds_phase_inc(duc1_phase_inc),
                   .data_out_i(dac1_data),
                   .data_out_q()
@@ -65,8 +109,8 @@ module rx_core
                 (
                   .clock(clock),
                   .resetn(resetn_reg),
-                  .data_in_i(dac2_data_i_temp1),
-                  .data_in_q(dac2_data_q_temp1),
+                  .data_in_i(lpf2_data2_i),
+                  .data_in_q(lpf2_data2_q),
                   .dds_phase_inc(duc2_phase_inc),
                   .data_out_i(dac2_data),
                   .data_out_q()
@@ -76,8 +120,8 @@ module rx_core
                 (
                   .clock(clock),
                   .resetn(resetn_reg),
-                  .data_in_i(dac3_data_i_temp1),
-                  .data_in_q(dac3_data_q_temp1),
+                  .data_in_i(lpf2_data3_i),
+                  .data_in_q(lpf2_data3_q),
                   .dds_phase_inc(duc3_phase_inc),
                   .data_out_i(dac3_data),
                   .data_out_q()
