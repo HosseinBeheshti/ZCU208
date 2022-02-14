6c0dea13c19da085d2a9ec39106a9b4b46019e23
HosseinBeheshti <h.beheshti92@gmail.com>
2022-02-14 17:21:29 +0330

    add sysgen fir ip cores

--- matlab/rx_dsp_core/rx_dsp_core_init_hw.m
+++ matlab/rx_dsp_core/rx_dsp_core_init_hw.m
@@ -1,22 +1,11 @@
 clear;
 close all;
 clc;
-sim_time = 1e-3;
+sim_time = 1e-7;
 %%
 fs = 500e6;
 ts = 1/fs;
-%% Hardware parameters
-ddc_phase_width = 16;
-ddc_data_width = 16;
-lpf1_outpu_width = 16;
-lpf1_outpu_point = 0;
-lpf2_outpu_width = 16;
-lpf2_outpu_point = 0;
-%% write xilinx coef files 
-hq = lpf1; 
-set(hq,'arithmetic','fixed'); 
-coewrite(hq,10,'coef_filter1');
-hq = lpf2; 
-set(hq,'arithmetic','fixed'); 
-coewrite(hq,10,'coef_filter2');
+%% filter coefficient
+lpf1_coefficient = lpf1;
+lpf2_coefficient = lpf2;
 
