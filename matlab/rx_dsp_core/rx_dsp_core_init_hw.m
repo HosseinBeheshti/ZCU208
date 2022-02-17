9c43b154c59bb879be05ac8ecb4e49b72298006d
HosseinBeheshti <h.beheshti92@gmail.com>
2022-02-17 16:39:29 +0330

    final sysgen model

--- matlab/rx_dsp_core/rx_dsp_core_init_hw.m
+++ matlab/rx_dsp_core/rx_dsp_core_init_hw.m
@@ -5,8 +5,12 @@ sim_time = 1e-7;
 %%
 fs = 500e6;
 ts = 1/fs;
-%% filter coefficient
+%% filter coefficient for dsp core
 lpf1_coefficient = lpf1;
 lpf2_coefficient = lpf2;
+%% filter coefficient for channelized receiver
+lpf3_coefficient = lpf3;
+lpf4_coefficient = lpf4;
+%% bit width
 ddc_phase_width = 16;
 ddc_data_width = 16;
