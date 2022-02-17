cd351ddc5655b912107680d812460f327bf6266b
HosseinBeheshti <h.beheshti92@gmail.com>
2022-02-17 17:34:26 +0330

    fix signaling error

--- hdl/parallel_data_slice.v
+++ hdl/parallel_data_slice.v
@@ -32,7 +32,7 @@ module parallel_data_slice
   );
 
   assign data_line_1 = data[16*(1)-1:16*(0)];
-  assign data_line_1 = data[16*(2)-1:16*(1)];
+  assign data_line_2 = data[16*(2)-1:16*(1)];
   assign data_line_3 = data[16*(3)-1:16*(2)];
   assign data_line_4 = data[16*(4)-1:16*(3)];
   assign data_line_5 = data[16*(5)-1:16*(4)];
