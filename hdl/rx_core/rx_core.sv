959ea212072bd0a42b38a01082f9591495a51e0e
HosseinBeheshti <h.beheshti92@gmail.com>
2022-03-03 19:06:11 +0330

    add input gateway

--- hdl/rx_core/rx_core.sv
+++ hdl/rx_core/rx_core.sv
@@ -40,19 +40,30 @@ module rx_core
    );
 
   genvar i;
+  logic [15:0] adc_data_array[NUMBER_OF_LINE];
   logic [15:0] dac1_data_re_array[NUMBER_OF_LINE];
   logic [15:0] dac2_data_re_array[NUMBER_OF_LINE];
   logic [15:0] dac3_data_re_array[NUMBER_OF_LINE];
 
+  generate
+    for (i = 0; i < NUMBER_OF_LINE; i = i + 1)
+    begin
+      always @(posedge clock)
+      begin
+        adc_data_array[i] <= adc_data[16*(i+1)-1:16*i];
+      end
+    end
+  endgenerate
+
   centeral_rx_dsp_core centeral_rx_dsp_core_inst (
-                         .adc_data0(adc_data[16*(1)-1:16*(0)]),
-                         .adc_data1(adc_data[16*(2)-1:16*(1)]),
-                         .adc_data2(adc_data[16*(3)-1:16*(2)]),
-                         .adc_data3(adc_data[16*(4)-1:16*(3)]),
-                         .adc_data4(adc_data[16*(5)-1:16*(4)]),
-                         .adc_data5(adc_data[16*(6)-1:16*(5)]),
-                         .adc_data6(adc_data[16*(7)-1:16*(6)]),
-                         .adc_data7(adc_data[16*(8)-1:16*(7)]),
+                         .adc_data0(adc_data_array[0]),
+                         .adc_data1(adc_data_array[1]),
+                         .adc_data2(adc_data_array[2]),
+                         .adc_data3(adc_data_array[3]),
+                         .adc_data4(adc_data_array[4]),
+                         .adc_data5(adc_data_array[5]),
+                         .adc_data6(adc_data_array[6]),
+                         .adc_data7(adc_data_array[7]),
                          .ddc_phase_inc(ddc_phase_inc),
                          .demix_gain(demix_gain),
                          .demix_phase_inc(demix_phase_inc),
